e87687fb8f42dfca96bd77df7206b6b9d9f7564e
Sharon <Sharonaquilla28@gmail.com>
2024-08-08 10:45:54 +0300

    adds initializing extensions

--- server/app.py
+++ server/app.py
@@ -1,20 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+from flask_restful import Api, Resource
+from dotenv import load_dotenv
+import os
+from models import db, User, WorkoutPlan, NutritionPlan, ProgressTracking
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'your_secret_key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
-db = SQLAlchemy(app)
+# Initialize extensions
+db.init_app(app)
+bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
+CORS(app, supports_credentials=True)
+api = Api(app)  # Initialize Flask-RESTful API
 
-# Import your models here
-import models
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(debug=True)
